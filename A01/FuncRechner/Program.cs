8a5c998fce98a74b73a71b03686ceb473debaee7
Konstantin Ibadullaev <konstantin.ibadullaev.post@gmail.com>
2024-04-27 18:19:46 +0200

    FuncRechner

---
+++ A01/FuncRechner/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace FuncRechner
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter a");
+           double a = Convert.ToDouble(ReadLine());
+
+            WriteLine("Enter e");
+            double e = Convert.ToDouble(ReadLine());
+
+            WriteLine("Enter s");
+            double s = Convert.ToDouble(ReadLine());
+
+            double ListsLen = (int) (e - a)/s;
+
+            List<double> xvals = new List<double>();
+            xvals.Add(a);
+
+            List<double?> fvals = new List<double?>();
+            WriteLine(ListsLen);
+            
+            WriteLine("| {0,-5} | {1,-5} |", " X ", " F(X) ");
+            for (int i = 0; i <= ListsLen; i++) 
+            {
+                xvals.Add(xvals[i] + s);
+
+                if ( (xvals[i] == -2.0) | (xvals[i] == 1) )
+                {
+                    fvals.Add(null);
+                    WriteLine("| {0,-5} | {1,-5} |", xvals[i], "NaN");
+                }
+                else
+                {
+                   
+                    double iexp = (3 * xvals[i] - 6) / ((xvals[i] + 2) * Math.Pow(xvals[i] - 1, 2));
+                    fvals.Add(iexp);
+                    WriteLine("| {0,-5} | {1,-5} |", xvals[i],fvals[i]);
+                    
+                }
+           
+
+            };
+
+
+
+        }
+    }
+}
