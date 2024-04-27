b58b72a0abe2639b9ff3f20688ad95dcd6db3b22
Konstantin Ibadullaev <konstantin.ibadullaev.post@gmail.com>
2024-04-27 17:30:42 +0200

    A1

---
+++ A01/MobileFunk/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace MobileFunk
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Tarif MyTarif = new Tarif();
+           
+            try
+            { 
+                WriteLine("Geben Sie Minutenmenge ein");
+                MyTarif.MinutenMenge = Convert.ToDouble(ReadLine());
+
+                WriteLine("Geben Sie Datenvolumen  ein: S,M,L");
+                MyTarif.DatenVolumen = Convert.ToChar(ReadLine().ToUpper());
+            } 
+            catch (IOException ex)
+            {
+                WriteLine($"Ausnahme sagt {ex.Message}");
+            }
+            catch (ArgumentNullException ex)
+            {
+                WriteLine($"Ausnahme sagt {ex.Message}");
+                
+            }
+            
+            catch (FormatException ex)
+            {
+                WriteLine($"Falsche Eingabe. Ausnahme sagt {ex.Message}");
+            }
+            catch(Exception ex)
+            {
+                WriteLine($"Ausnahme sagt {ex.Message}");
+            }
+            finally
+            {
+                WriteLine($"Ihre Mobilefunkkosten für {MyTarif.MinutenMenge} Minuten betragen {MyTarif.MobileKostenRechnen()} Euro");
+
+                WriteLine($"Ihre Internetkosten für Paket {MyTarif.DatenVolumen} betragen {MyTarif.InternetKostenRechnen()} Euro");
+
+
+                WriteLine($"Ihre totale Kosten betragen {MyTarif.TotalKostenRechnen()} Euro");
+            }
+
+
+            
+
+        }
+    }
+}
---
+++ A01/MobileFunk/Tarif.cs
@@ -0,0 +1,78 @@
+﻿
+namespace MobileFunk
+{
+
+    public class Tarif
+    {
+        public static double MonGebuer = 3.99;
+        public static double KostenProMinute = 0.099;
+        public static Dictionary<char, double> InternetPaket = new Dictionary<char, double>() { { 'S', 5 }, { 'M', 7 }, { 'L', 10 } };
+
+
+        public double MinutenMenge
+        {
+            get;
+            set;
+        }
+
+
+        public char  DatenVolumen
+        {
+            get;
+            set;
+        }
+
+        public readonly double MinutenPaket = 100;
+
+
+        public Tarif() { }
+
+
+        public double MobileKostenRechnen()
+        {
+            double TotKost;
+            if (MinutenMenge <= MinutenPaket)
+            {
+                TotKost = MonGebuer;
+
+            }
+            else
+            {
+                TotKost = MonGebuer + KostenProMinute * (MinutenMenge - MinutenPaket);
+            }
+            return TotKost;
+
+        }
+
+
+        public double InternetKostenRechnen()
+        {
+
+            
+                if (InternetPaket.ContainsKey( DatenVolumen))
+                {
+                    return InternetPaket[DatenVolumen];
+                }
+                else
+                {
+                    WriteLine($"Falsche Option");
+                    return 0;
+                }
+            
+           
+
+        }
+
+
+
+
+        public double TotalKostenRechnen()
+        {
+
+
+
+            return MobileKostenRechnen() + InternetKostenRechnen();
+
+        }
+    }
+}
