2a5ece62aa65061fa98d8b9db9d2f1af82ef8081
Konstantin Ibadullaev <konstantin.ibadullaev.post@gmail.com>
2024-04-27 18:51:27 +0200

    FuncRechner corrected

--- A01/FuncRechner/Program.cs
+++ A01/FuncRechner/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             WriteLine("Enter a");
-           double a = Convert.ToDouble(ReadLine());
+            double a = Convert.ToDouble(ReadLine());
 
             WriteLine("Enter e");
             double e = Convert.ToDouble(ReadLine());
@@ -13,32 +13,34 @@
             WriteLine("Enter s");
             double s = Convert.ToDouble(ReadLine());
 
-            double ListsLen = (int) (e - a)/s;
+            double ListsLen = Math.Round( (e - a)/s, 0, MidpointRounding.AwayFromZero);
 
-            List<double> xvals = new List<double>();
-            xvals.Add(a);
+            List<double> xvals = new List<double>() { a,};
+            //xvals.Add(a);
 
             List<double?> fvals = new List<double?>();
-            WriteLine(ListsLen);
+            //WriteLine(ListsLen);
             
-            WriteLine("| {0,-5} | {1,-5} |", " X ", " F(X) ");
+            WriteLine("| {0,10} | {1,-10} |", " X ", "F(X) ");
+            WriteLine("---------------------------");
             for (int i = 0; i <= ListsLen; i++) 
             {
-                xvals.Add(xvals[i] + s);
+                
 
                 if ( (xvals[i] == -2.0) | (xvals[i] == 1) )
                 {
                     fvals.Add(null);
-                    WriteLine("| {0,-5} | {1,-5} |", xvals[i], "NaN");
+                    WriteLine("| {0,10:F3} | {1,-10:F3} |", xvals[i], "NaN");
                 }
                 else
                 {
                    
                     double iexp = (3 * xvals[i] - 6) / ((xvals[i] + 2) * Math.Pow(xvals[i] - 1, 2));
                     fvals.Add(iexp);
-                    WriteLine("| {0,-5} | {1,-5} |", xvals[i],fvals[i]);
+                    WriteLine("| {0,10:F3} | {1,-10:F3} |", xvals[i],fvals[i]);
                     
                 }
+                xvals.Add(xvals[i] + s);
            
 
             };
